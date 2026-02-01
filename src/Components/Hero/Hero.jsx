2a5a5d43110b015ef935a9c327a5c5d30b10714f
mrudul2904 <mehtamrudul21mm@gmail.com>
2026-02-01 17:32:56 +0530

    Replace old resume with updated resume

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-scroll';
 import './Hero.css'
 import profile_img from '../../assets/profile_img.jpg'
-import myResume from '../../assets/Resume_Mrudul_Kumar_Mehta.pdf';
+import myResume from '../../assets/0801EC221057_BT_ET_Mrudul_Kumar_Mehta';
 
 const Hero = () => {
     return(
